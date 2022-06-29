0560818e8a617fc38fe12d0750e7a8bdae1d7e07
Gabriel Do Carmo <83257171+G7Carmo@users.noreply.github.com>
2022-06-28 21:29:04 -0300

    intro Activity configurrada

---
+++ app/src/main/java/com/gdsdesenvolvimento/organizecontas/ui/view/IntroductionActivity.kt
@@ -0,0 +1,10 @@
+package com.gdsdesenvolvimento.organizecontas.ui.view
+
+import android.os.Bundle
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
+
+class IntroductionActivity : IntroActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
